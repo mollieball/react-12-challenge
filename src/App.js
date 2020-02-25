76401d7095e500ef06579b410f2c9f69b85070f5
Mollie Ball <mollieball@squarepeg.ie>
2020-02-25 11:03:00 +0000

    Complete react twelve quest

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import TabSelector from './TabSelector';
-import './App.css';
+import React, { Component } from "react";
+import TabSelector from "./TabSelector";
+import "./App.css";
 
 const Home = () => <p>This is the Home component!</p>;
 const About = () => <p>This challenge is about tabs!</p>;
 const Contact = () => <p>Feel free to contact us!</p>;
-const Error = () => <p>Something went <strong>wrong</strong>!</p>;
+const Error = () => (
+  <p>
+    Something went <strong>wrong</strong>!
+  </p>
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeId: 'home'
+      activeId: "home"
     };
+    this.tabChange = this.tabChange.bind(this);
   }
 
   getTabContent() {
-    switch(this.state.activeId) {
-      case 'home':
+    switch (this.state.activeId) {
+      case "home":
         return <Home />;
+      case "about":
+        return <About />;
+      case "contact":
+        return <Contact />;
       default:
         return <Error />;
     }
   }
-
+  tabChange(buttonId) {
+    this.setState({ activeId: buttonId });
+  }
   render() {
     return (
       <div className="App">
-        <TabSelector />
-        <div className="App-content">
-          {
-            this.getTabContent()
-          }
-        </div>
+        <TabSelector
+          activeId={this.state.activeId}
+          tabChange={this.tabChange}
+        />
+        <div className="App-content">{this.getTabContent()}</div>
       </div>
     );
   }
--- src/TabSelector.js
+++ src/TabSelector.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TabSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeId: 'home'
-    };
+    this.state = {};
     this.handleChangeTab = this.handleChangeTab.bind(this);
   }
 
   handleChangeTab(event) {
     const buttonId = event.target.id;
-    this.setState({ activeId: buttonId });
+    this.props.tabChange(buttonId);
   }
 
   render() {
-    const { activeId } = this.state;
+    const { activeId } = this.props;
     return (
       <div className="TabSelector">
         <button
           id="home"
           onClick={this.handleChangeTab}
-          className={activeId === 'home' ? 'active' : ''}
+          className={activeId === "home" ? "active" : ""}
         >
           Home
         </button>
         <button
           id="about"
           onClick={this.handleChangeTab}
-          className={activeId === 'about' ? 'active' : ''}
+          className={activeId === "about" ? "active" : ""}
         >
           About
         </button>
         <button
           id="contact"
           onClick={this.handleChangeTab}
-          className={activeId === 'contact' ? 'active' : ''}
+          className={activeId === "contact" ? "active" : ""}
         >
           Contact
         </button>
